5ba3d72b4b919e8775fbbbee36ddea154817ae05
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-03-26 10:49:43 +0100

    Modified example to fit syntax

--- example.py
+++ example.py
@@ -1,20 +1,11 @@
 #!/usr/env python
 
 from picas.actors import RunActor
-from picas.clients import CouchClient
-from picas.iterators import BasicViewIterator
-from picas.modifiers import BasicTokenModifier
+from picas.clients import CouchDB
 
 class ExampleActor(RunActor):
-    def __init__(self, iterator, modifier):
-        #RunActor.__init__(iterator, modifier)
-        # This is what happens in RunActor:
-        self.iterator = iterator
-        self.client = iterator.client
-        self.modifier = modifier
-
-        # Make a copy of the db reference
-        self.db = iterator.client.db
+    def __init__(self, db, view, **viewargs):
+        super(ExampleActor, self).__init__(db, view, **viewargs)
 
     def prepare_env(self, *kargs, **kvargs):
         pass
@@ -23,25 +14,19 @@ class ExampleActor(RunActor):
         pass
 
     def process_token(self, ref, token):
-        # this is where all the work gets done. Start editing here.
-        
+        # this is where all the work gets done. Start editing here.        
         print token
         
-        token = self.modifier.close(token)
-        self.db[ref[0]] = token
-
-
     def cleanup_run(self, *kargs, **kvargs):
         pass
 
     def cleanup_env(self, *kargs, **kvargs):
         pass
 
+
 def main():
-    client = CouchClient(url="http://localhost:5984", db='test')
-    modifier = BasicTokenModifier()
-    iterator = BasicViewIterator(client, "example/todo", modifier)
-    actor = ExampleActor(iterator, modifier)
+    db = CouchDB(url="http://localhost:5984", db='test')
+    actor = ExampleActor(db, 'todo')
     actor.run()
 
 if __name__ == '__main__':
