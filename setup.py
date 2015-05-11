90e2ce0f6ca21575b73f0d8f2207ca30b3a46414
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-05-11 13:01:12 +0200

    Version 0.2.2.1: SSL verification fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 
 setup(name='picas',
-      version='0.2.2',
+      version='0.2.2.1',
       description='Python client using CouchDB as a token pool server.',
       author='Jan Bot',
       author_email='jan.bot@surfsara.nl',
